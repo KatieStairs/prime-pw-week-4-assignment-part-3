1a9c67dcd4e83c0931aa1e25c6b747d13c47921b
Katie Stairs <kstairs831@gmail.com>
2022-11-20 14:06:52 -0600

    Completed all parts

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,34 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+    console.log('in addItem, added', item);
+    basket.push(item);
+    return true;
+}
+
+console.log(addItem('blueberries'));
+console.log(addItem('peaches'));
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+basket.forEach(function (item) {
+    console.log(item);
+});
+
+
+function empty(basket){
+    console.log('start empty', basket);
+    while (basket.length > 0){
+        console.log('in empty', basket);
+        basket.pop();
+    }
+    return true;
+}
+
+console.log('Basket empty', empty(basket));
